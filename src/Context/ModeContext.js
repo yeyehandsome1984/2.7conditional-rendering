94dc3a48d3fc822ed945605de930752f8f437ef9
Yichen <53641179+yeyehandsome1984@users.noreply.github.com>
2024-07-04 07:24:35 +0800

    initial push

---
+++ src/Context/ModeContext.js
@@ -0,0 +1,23 @@
+import { createContext, useState } from 'react';
+
+const ModeContext = createContext();
+
+export function ModeProvider({ children }) {
+  
+  const [isLight, setIsLight] = useState(true);
+  
+  const handlerToggle = () => {
+    setIsLight(() => !isLight);
+  }
+  
+  const context = { 
+    isLight: isLight,
+    handlerToggle: handlerToggle, 
+  }
+  return (
+    <ModeContext.Provider value={context}>
+      {children}
+    </ModeContext.Provider>
+  )
+}
+export default ModeContext;
